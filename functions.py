272ca7ea965f02635544608e59e8dbcb5732366b
vivighi <vivdumitrescu@gmail.com>
2023-01-16 14:20:45 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list
+     of to-do items.
+     """
+    with open(filepath, 'r') as file:
+        todos = file.readlines()
+    return todos
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write a to-do items list in the text file. """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("hello")
